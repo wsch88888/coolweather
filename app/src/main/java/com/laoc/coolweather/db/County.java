cdafd136677df577d3259c3a30911f69aa03f7a2
chenhao7 <chenhao7@tcl.com>
2018-04-03 16:11:05 +0800

    create db

---
+++ app/src/main/java/com/laoc/coolweather/db/County.java
@@ -0,0 +1,44 @@
+package com.laoc.coolweather.db;
+
+/**
+ * Created by chenhao7 on 2018/4/3.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
